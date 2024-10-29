b579780e5bee749661fb21cd5a29fa962733e912
william0814 <king-master90@hotmail.com>
2024-10-29 22:53:49 +0100

    mas codigo

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const boton = document.getElementById('miBoton');
 boton.addEventListener('click', () => {
   alert('Has hecho clic en el boton');
 });
+
+var nombre = prompt('Ingresesu nombre:', 'william');
+
+alert('mensaje');
+var confirmacion = confirm('Estas seguro?');
+if ((confirmacion = true)) {
+  prompt('correcto', 'correcto');
+}
+if ((confirmacion = false)) {
+  prompt('incorrecto', 'incorrecto');
+}
+console.log('Mensaje por consola');
